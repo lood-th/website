fc1c8b17c981d7960a1396717722a68a04f9de58
lood-th <email@ludwigthormann.de>
2021-04-25 22:24:32 +0200

    setup api experiments

--- contents/script.js
+++ contents/script.js
@@ -1,9 +1,20 @@
 
-const makeAppear = () => {
-    setTimeout(() => {
-    let emailButton = document.getElementById(`email`);
-       emailButton.classList.add(`vis`);
-    }, 500);
+const makeAppear = (elem) => {
+     setTimeout(() => {
+       elem.classList.add(`vis`);
+    }, 500); 
 }
 
-makeAppear();
+let emailButton = document.querySelector(`#email`); 
+makeAppear(emailButton);
+
+console.log("Rest of the script is being executed :)))")
+
+let weatherDisplay = document.querySelector('#weather-display');
+fetch('http://style.css')
+.then(response => {
+    weatherDisplay.innerHTML = `<p>${response}</p>`;
+})
+.catch(
+    err => {weatherDisplay.innerHTML = `<p>${err}</p>`;}
+);
